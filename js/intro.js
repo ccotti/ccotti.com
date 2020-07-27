b0621761011eb35fa669ad21daff1b2e2c66d254
Chris Cottingham <chris@ccotti.com>
2020-07-26 19:24:06 -0500

    initial commit of files

---
+++ js/intro.js
@@ -0,0 +1,35 @@
+<!-- Begin HEADER OPTIONS CODE
+
+// ONLY USE lowercase FOR ALL OPTIONS
+
+
+// CHANGE ANY OF THESE VARIABLES TO "no" OR "yes" TO TURN AN OPTION OFF OR ON
+
+
+var flashwidth		= "700"		// WIDTH OF THE FLASH (IN PIXELS)
+var flashheight		= "300"		// HEIGHT OF THE FLASH (IN PIXELS)
+var color		= "000000"	// INTRO BACKGROUND COLOR
+
+
+
+
+// COPYRIGHT 2006 © Allwebco Design Corporation
+// Unauthorized use or sale of this script is strictly prohibited by law
+
+// YOU DO NOT NEED TO EDIT BELOW THIS LINE
+
+
+
+
+document.write('<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0" WIDTH="'+flashwidth+'" HEIGHT="'+flashheight+'" id="intro">');
+document.write('<PARAM NAME="movie" VALUE="intro.swf">');
+document.write('<PARAM NAME="quality" VALUE="high">');
+document.write('<PARAM NAME="wmode" VALUE="transparent">');
+document.write('<PARAM NAME="bgcolor" VALUE="#'+color+'">');
+document.write('<EMBED src="flash/intro.swf" quality="high" wmode="transparent" bgcolor="#'+color+'"  WIDTH="'+flashwidth+'" HEIGHT="'+flashheight+'" NAME="intro" TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED></OBJECT><br>');
+
+
+
+
+
+//  End -->
